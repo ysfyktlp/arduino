a1d6bb35d5f5b53c22a5d4b0fe5437c271e2b9a5
ysfyktlp <33184169+ysfyktlp@users.noreply.github.com>
2018-03-23 22:34:23 +0300

    Add files via upload

---
+++ cicek_sulama_sistemi.ino
@@ -0,0 +1,35 @@
+int sensor = A0;
+int motor = 4 ;
+
+int veri;
+
+
+void setup() {
+  pinMode(sensor,INPUT);
+  pinMode(motor,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  veri=analogRead(sensor);
+  Serial.println(veri);
+
+if(veri>600)
+{
+  digitalWrite(motor,HIGH);
+  delay(3000);
+  digitalWrite(motor,LOW);
+  delay(3000);
+  veri=analogRead(sensor);
+  if(veri>600){
+    digitalWrite(motor,HIGH);
+    delay(2000);
+    digitalWrite(motor,LOW);
+    delay(5000);
+    }else {
+      digitalWrite(motor,LOW);
+      }
+  }else{
+    digitalWrite(motor,LOW);
+    }
+}
