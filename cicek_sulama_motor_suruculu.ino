f68ba062fb87018273b10670b11260827a2f4821
ysfyktlp <33184169+ysfyktlp@users.noreply.github.com>
2018-03-28 22:59:52 +0300

    çiçek sulama sistemi motor sürücülü

---
+++ cicek_sulama_motor_suruculu.ino
@@ -0,0 +1,47 @@
+const int in1=3;
+const int in2=4;
+
+int sensor = A0;
+int motor = 5 ;
+int veri;
+
+
+void setup() {
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(sensor,INPUT);
+  pinMode(motor,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  veri=analogRead(sensor);
+  Serial.println(veri);
+
+if(veri>700)
+{
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  delay(4000);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  delay(4000);
+  veri=analogRead(sensor);
+  if(veri>700){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    delay(4000);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+    delay(7000);
+    }else {
+      digitalWrite(in1,LOW);
+      digitalWrite(in2,LOW);
+      }
+  }else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+    }
+
+
+}
