7d7407e504f599081e315e1ec7a88990ebeca57f
ysfyktlp <33184169+ysfyktlp@users.noreply.github.com>
2018-05-05 15:36:35 +0300

    Update dht11_ve_lcd.ino

--- dht11_ve_lcd.ino
+++ dht11_ve_lcd.ino
@@ -6,8 +6,7 @@ dht11 DHT11;
 
 void setup()
 {
-  //Serial.begin(9600); // Seri iletişimi başlatıyoruz.
-  //Serial.println("bilisimderslerim.com");
+
   lcd.begin(16, 2);
 }
 
@@ -21,26 +20,6 @@ void loop()
   // chk değerini serial monitörde yazdırıp kontrol edebilirsiniz.
   int chk = DHT11.read(DHT11PIN);
 
-  /*Sensörden gelen verileri serial monitörde yazdırıyoruz.
-  Serial.print("Nem (%): ");
-  Serial.println((float)DHT11.humidity, 2);
-
-  Serial.print("Sicaklik (Santigrat): ");
-  Serial.println((float)DHT11.temperature, 2);
-
-  Serial.print("Sicaklik (Fahrenheit): ");
-  Serial.println(DHT11.fahrenheit(), 2);
-
-  Serial.print("Sicaklik (Kelvin): ");
-  Serial.println(DHT11.kelvin(), 2);
-
-  // Çiğ Oluşma Noktası, Dew Point
-  Serial.print("Cig Olusma Noktasi: ");
-  Serial.println(DHT11.dewPoint(), 2);
-
-  // 2 saniye bekliyoruz. 2 saniyede bir veriler ekrana yazdırılacak.
-  delay(2000); */
-
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Sicaklik: ");
@@ -49,27 +28,13 @@ void loop()
   lcd.print("Derece"); 
    
   delay(3000);
+  
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Nem: ");
-  
   lcd.print("Yuzde "); 
   lcd.print((float)DHT11.humidity);
    
-
   delay(3000);
   
-  /*lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Fahrenheit ");
-  lcd.print((float)DHT11.fahrenheit());
-  lcd.print(" D");  
-  
-  lcd.setCursor(0,1);
-  lcd.print("Kelvin: ");
-  lcd.print((float)DHT11.kelvin());  
-  lcd.print(" D");
-
-  delay(3000);
-  lcd.clear();*/
 }
