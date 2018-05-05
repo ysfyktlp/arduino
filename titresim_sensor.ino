f5a44ca88a7929d2a6b121a7c609a3c300043e5a
ysfyktlp <33184169+ysfyktlp@users.noreply.github.com>
2018-05-05 14:49:21 +0300

    Add files via upload

---
+++ titresim_sensor.ino
@@ -0,0 +1,23 @@
+int veri;
+
+int led=4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(veri,INPUT);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  veri =digitalRead(2);
+  Serial.print(veri);
+  Serial.println("titresim değeri : ");
+
+  if(veri==1){
+    digitalWrite(led,HIGH);
+    }else{
+      digitalWrite(led,LOW);
+      }
+
+}
