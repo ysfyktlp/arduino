587252d03127e14d8a4912f6f0f8b829198a2d06
ysfyktlp <33184169+ysfyktlp@users.noreply.github.com>
2018-05-05 15:13:05 +0300

    Sıcaklık Nem Sensörü kodları

---
+++ dht11.ino
@@ -0,0 +1,45 @@
+
+#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
+#define DHT11PIN 2 // DHT11PIN olarak Dijital 2'yi belirliyoruz.
+
+dht11 DHT11;
+
+void setup()
+{
+  Serial.begin(9600); // Seri iletişimi başlatıyoruz.
+  Serial.println("bilisimderslerim.com");
+ 
+}
+
+void loop()
+{
+  
+  // Bir satır boşluk bırakıyoruz serial monitörde.
+
+  // Sensörün okunup okunmadığını konrol ediyoruz. 
+  // chk 0 ise sorunsuz okunuyor demektir. Sorun yaşarsanız
+  // chk değerini serial monitörde yazdırıp kontrol edebilirsiniz.
+  int chk = DHT11.read(DHT11PIN);
+
+  //Sensörden gelen verileri serial monitörde yazdırıyoruz.
+  Serial.print("Nem (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Sicaklik (Santigrat): ");
+  Serial.println((float)DHT11.temperature, 2);
+
+  Serial.print("Sicaklik (Fahrenheit): ");
+  Serial.println(DHT11.fahrenheit(), 2);
+
+  Serial.print("Sicaklik (Kelvin): ");
+  Serial.println(DHT11.kelvin(), 2);
+
+  // Çiğ Oluşma Noktası, Dew Point
+  Serial.print("Cig Olusma Noktasi: ");
+  Serial.println(DHT11.dewPoint(), 2);
+
+  // 2 saniye bekliyoruz. 2 saniyede bir veriler ekrana yazdırılacak.
+  delay(2000); 
+
+  
+}
